6717e3ddfbdede3a6db83a7253b803e6f8ba3f70
sunui <776766759@qq.com>
2017-10-14 19:09:06 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+#dev 新增模块
+webpack.config.dev.js
+webpackDevServer.config
+搜索dashboard
+
+#build 新增模块
+webpack.config.prod.js
+
+
+##目前模块
+
+- dashboard
+- instances
+- processes
+- template
---
+++ src/dashboard/App2.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       dashboard
+      </div>
+    );
+  }
+}
+
+export default App;
